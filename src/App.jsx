01aa731743106d2aae07b79e5bb981629edcbdac
Taonga Chiwowa <tlytons@gmail.com>
2026-08-13 07:45:56 +0200

    chunking fix and possibly routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,102 +1,155 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import ProjectDetails from "./pages/ProjectDetails";
-import ProjectsView from "./pages/ProjectsView";
-import Login from "./pages/Login";
-import AdminDashboard from "./pages/AdminDashboard";
-import AdminRoute from "./components/AdminRoute";
-import AdminLayout from "./admin/AdminLayout";
-import AdminProjects from "./admin/AdminProjects";
-import AdminProjectCreate from "./admin/AdminProjectCreate";
-import AdminProjectEdit from "./admin/AdminProjectEdit";
-import AdminSkills from "./admin/AdminSkills";
-import AdminSkillForm from "./admin/AdminSkillForm";
-import AdminTechnologies from "./admin/AdminTechnologies";
-import AdminTechnologyForm from "./admin/AdminTechnologiesForm";
-import AdminResumes from "./admin/AdminResume";
-import AdminProfile from "./admin/AdminProfile";
+import { lazy,Suspense } from "react";
+
+const Home = lazy(()=>
+  import("./pages/Home")
+);
+const ProjectDetails = lazy(()=>
+  import("./pages/ProjectDetails")
+);
+const ProjectsView = lazy(()=>
+  import("./pages/ProjectsView")
+);
+const Login = lazy(()=>
+  import("./pages/Login")
+);
+const AdminDashboard = lazy(()=>
+  import("./pages/AdminDashboard")
+);
+const AdminRoute = lazy(()=>
+  import("./components/AdminRoute")
+);
+const AdminLayout = lazy(()=>
+  import("./admin/AdminLayout")
+);
+const AdminProjects = lazy(()=>
+  import("./admin/AdminProjects")
+);
+const AdminProjectCreate = lazy(()=>
+  import("./admin/AdminProjectCreate")
+);
+const  AdminProjectEdit = lazy(()=>
+  import("./admin/AdminProjectEdit")
+);
+const  AdminSkills = lazy(()=>
+  import("./admin/AdminSkills")
+);
+const  AdminSkillForm = lazy(()=>
+  import("./admin/AdminSkillForm")
+);
+const  AdminTechnologies = lazy(()=>
+  import("./admin/AdminTechnologies")
+);
+const  AdminTechnologyForm = lazy(()=>
+  import("./admin/AdminTechnologiesForm")
+);
+const  AdminResumes = lazy(()=>
+  import("./admin/AdminResume")
+);
+const  AdminProfile = lazy(()=>
+  import("./admin/AdminProfile")
+);
+
+
 
 function App(){
   return(
     <BrowserRouter>
-    <Routes>
-      <Route
-      path="/"
-      element={<Home/>}
-      />
-      <Route
-      path="/project/:id"
-      element={<ProjectDetails/>}
-      />
-      <Route
-      path="/projects"
-      element={<ProjectsView/>}
-      />
-      <Route
-      path="/admin/login"
-      element={<Login/>}
-      />
-      <Route
-      element={<AdminRoute/>}
-      >
+    <Suspense
+      fallback={
+        <div className="
+        min-h-screen
+        flex
+        items-center
+        justify-center
+        bg-slate-950
+        text-white
+        "
+        >
+          Loading....
+        </div>
+      }
+    >
+      <Routes>
         <Route
-        element={<AdminLayout/>}
+        path="/"
+        element={<Home/>}
+        />
+        <Route
+        path="/project/:id"
+        element={<ProjectDetails/>}
+        />
+        <Route
+        path="/projects"
+        element={<ProjectsView/>}
+        />
+        <Route
+        path="/admin/login"
+        element={<Login/>}
+        />
+        <Route
+        element={<AdminRoute/>}
         >
           <Route
-          path="/admin"
-          element={<AdminDashboard/>}
-          />
-          <Route
-            path="/admin/projects"
-            element={<AdminProjects />}
-          />
-          <Route
-            path="/admin/projects/create"
-            element={<AdminProjectCreate />}
-          />
-          <Route
-            path="/admin/projects/:id/edit"
-            element={<AdminProjectEdit />}
-          />
-          <Route
-            path="/admin/skills"
-            element={<AdminSkills />}
-          />
-          <Route
-              path="/admin/skills/create"
-              element={<AdminSkillForm />}
-          />
+          element={<AdminLayout/>}
+          >
+            <Route
+            path="/admin"
+            element={<AdminDashboard/>}
+            />
+            <Route
+              path="/admin/projects"
+              element={<AdminProjects />}
+            />
+            <Route
+              path="/admin/projects/create"
+              element={<AdminProjectCreate />}
+            />
+            <Route
+              path="/admin/projects/:id/edit"
+              element={<AdminProjectEdit />}
+            />
+            <Route
+              path="/admin/skills"
+              element={<AdminSkills />}
+            />
+            <Route
+                path="/admin/skills/create"
+                element={<AdminSkillForm />}
+            />
 
-          <Route
-              path="/admin/skills/:id/edit"
-              element={<AdminSkillForm />}
-          />
-          <Route
-          path="/admin/technologies"
-          element={<AdminTechnologies />}
-          />
-          <Route
-            path="/admin/technologies/create"
-            element={<AdminTechnologyForm />}
-          />
-          <Route
-            path="/admin/technologies/:id/edit"
-            element={<AdminTechnologyForm />}
-          />
-          <Route
-            path="/admin/resume"
-            element={<AdminResumes />}
-          />
-          <Route
-          path="/admin/profile"
-          element={<AdminProfile />}
-          />
+            <Route
+                path="/admin/skills/:id/edit"
+                element={<AdminSkillForm />}
+            />
+            <Route
+            path="/admin/technologies"
+            element={<AdminTechnologies />}
+            />
+            <Route
+              path="/admin/technologies/create"
+              element={<AdminTechnologyForm />}
+            />
+            <Route
+              path="/admin/technologies/:id/edit"
+              element={<AdminTechnologyForm />}
+            />
+            <Route
+              path="/admin/resume"
+              element={<AdminResumes />}
+            />
+            <Route
+            path="/admin/profile"
+            element={<AdminProfile />}
+            />
+          </Route>
+          
         </Route>
         
-      </Route>
-      
-      
-    </Routes>
+        
+      </Routes>
+    </Suspense>
+    
     </BrowserRouter>
   )
 }
